b72a3fef7e3310e2d0ddee0d60c71a63449db287
弘树 <tiehang.lth@alibaba-inc.com>
2014-07-30 18:00:44 +0800

    sync daily version 2.0.0

--- 2.0.0/src/index.js
+++ 2.0.0/src/index.js
@@ -1,29 +1,15 @@
 /**
- * @fileoverview
- * @author
+ * @fileoverview 媒体查询假名组件
+ * @author 踏风<tafeng.dxx@taobao.com>
  * @module mqa
  **/
-KISSY.add(function (S, Node,Base) {
-    var EMPTY = '';
-    var $ = Node.all;
-    /**
-     *
-     * @class Mqa
-     * @constructor
-     * @extends Base
-     */
-    function Mqa(comConfig) {
-        var self = this;
-        //调用父类构造函数
-        Mqa.superclass.constructor.call(self, comConfig);
-    }
-    S.extend(Mqa, Base, /** @lends Mqa.prototype*/{
+;
+KISSY.add(function (S, Mqa) {
 
-    }, {ATTRS : /** @lends Mqa*/{
-
-    }});
-    return Mqa;
-}, {requires:['node', 'base']});
+    return Mqa
+}, {
+    requires: ["./mqa"]
+})
 
 
 
